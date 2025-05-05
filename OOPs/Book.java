3e3331950221f6df68b755e8941ecad2758da3f8
Yashaswi <115717868+LoopAndLearn-dev@users.noreply.github.com>
2025-05-05 20:20:22 +0530

    Added Encapsulation example

--- OOPs/Book.java
+++ OOPs/Book.java
@@ -1,1 +1,26 @@
+package encapsulation;
+class Book {
+	private int pages;
+
+	// setter method/mutators
+	public void setPages(int x) {
+		if (x > 0) {
+			pages = x;
+		}
+	}
+
+	// getter method/accessors
+	public int getPages() {
+		return pages;
+	}
+}
+
+public class BookApp {
+	public static void main(String[] args) {
+		Book b = new Book();
+		b.setPages(-100);
+		int m = b.getPages();
+		System.out.println("The value of pages are " + m);
+	}
+}
 
